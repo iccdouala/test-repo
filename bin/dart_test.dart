c027077e2fdda2de6156c0731f2c52b1994fe02a
Gildas AYEFOU <ayefougil@gmail.com>
2020-08-13 23:25:07 +0100

    first commit

---
+++ bin/dart_test.dart
@@ -0,0 +1,7 @@
+import 'package:dart_test/dart_test.dart' as dart_test;
+
+/// This is the main method
+void main(List<String> arguments) {
+  print('Hello world: ${dart_test.calculate()}!');
+  print(arguments);
+}
